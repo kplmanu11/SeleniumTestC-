278873bda40bf62d7819c54b9594f45dbf411553
Your Name <you@example.com>
2018-10-30 02:03:22 +0545

    all changes saved

---
+++ ReadExcelSelenium/Properties/AssemblyInfo.cs
@@ -0,0 +1,20 @@
+using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+[assembly: AssemblyTitle("ReadExcelSelenium")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("ReadExcelSelenium")]
+[assembly: AssemblyCopyright("Copyright ©  2018")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+[assembly: ComVisible(false)]
+
+[assembly: Guid("6114fc54-de48-496c-883b-932b58c80840")]
+
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
---
+++ ReadExcelSelenium/UnitTest1.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using OpenQA.Selenium;
+using OpenQA.Selenium.Chrome;
+using excel = Microsoft.Office.Interop.Excel;
+using System.Threading;
+
+
+namespace ReadExcelSelenium
+{
+	[TestClass]
+	public class UnitTest1
+	{
+		[TestMethod]
+		public void TestMethod1()
+		{
+			int iRow = 0;
+			int iCol = 0;
+
+			#region read excel file 
+			excel.Application exApp = new excel.Application();
+			excel.Workbook exWorkBok = exApp.Workbooks.Open(@"C:\Users\Manu\Desktop\Excel for Seva Development.xlsx");
+			excel._Worksheet exWorkShet = exWorkBok.Sheets[1];
+			excel.Range exRange = exWorkShet.UsedRange;
+			#endregion
+
+
+			for (iRow = 1; iRow <= exRange.Rows.Count; iRow++)
+			{
+				for (iCol = 1; iCol <= exRange.Columns.Count; iCol++)
+				{
+					IWebDriver driver = new ChromeDriver();
+					driver.Navigate().GoToUrl("https://www.google.com");
+
+					String value = exRange.Cells[iRow, iCol].Value2;
+					driver.FindElement(By.Id("lst-ib")).SendKeys(value);
+					driver.FindElement(By.Id("lst-ib")).SendKeys(Keys.Enter);
+					Thread.Sleep(50);
+					driver.Close();
+				}
+
+			}
+
+		}
+	}
+}
